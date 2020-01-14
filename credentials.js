1878dfbc6bd74e39e9259c5022bebb682a5af6f4
Mohammed Al Badaai <mohammednalbadaai@gmail.com>
2020-01-14 14:20:44 +0400

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key:         '9rqud8Z5FKraggNihSPPOikkj',
+	consumer_secret:      '2r7pin2E12KX9n5OJF883HKzIZHry9Os8RzDc3v69CrWf6Gz01',
+	access_token:         '1204843197899444226-MY61ijT6uSEPOo0iHtT2JsGpYtSchj',
+	access_token_secret:  'EtXO6tHTdYTqG9zlas9nzwbjPKKXbjGzGPIgPYnl16Lqr'
+}
